50ee02edff8a23481dbe0e47f13f69275db3ee05
Séverin Lemaignan <severin.lemaignan@plymouth.ac.uk>
2015-10-09 13:09:49 +0100

    Added a option to disable human's animations like the walk cycle
    Use example:
    human = Human()
    human.properties(Animations=False)

--- src/morse/robots/human.py
+++ src/morse/robots/human.py
@@ -2,12 +2,17 @@ import logging; logger = logging.getLogger("morse." + __name__)
 from morse.core import blenderapi
 from morse.robots.grasping_robot import GraspingRobot
 from morse.core.services import service
+from morse.helpers.components import add_property
 
 class Human(GraspingRobot):
     """ Class definition for the human as a robot entity.
 
     Sub class of GraspingRobot.
     """
+
+    add_property('_animations', True, 'Animations', 'bool', "if "
+            "true (default), will enable various animations like"
+            " the walk cycle animation.")
 
     def __init__(self, obj, parent=None):
         """ Call the constructor of the parent class """
@@ -55,13 +60,15 @@ class Human(GraspingRobot):
         WALK_END_FRAME = 32
 
         logger.warning("Applying speed " + str(linear_speed))
-        if linear_speed[0] != 0 or angular_speed[2] != 0:
-            self.armature.playAction("walk", 
-                                     WALK_START_FRAME, WALK_END_FRAME, 
-                                     speed=speed_factor)
-        else:
-            self.armature.playAction("walk", 
-                                     WALK_START_FRAME, WALK_START_FRAME)
+
+        if self._animations:
+            if linear_speed[0] != 0 or angular_speed[2] != 0:
+                self.armature.playAction("walk", 
+                                        WALK_START_FRAME, WALK_END_FRAME, 
+                                        speed=speed_factor)
+            else:
+                self.armature.playAction("walk", 
+                                        WALK_START_FRAME, WALK_START_FRAME)
 
 
         if kind == 'Position':
